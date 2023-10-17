b187088a6e84b25b2434d99ce13975318db43c3c
fornara <fornarapro@gmail.com>
2023-10-17 14:46:58 +0300

    webpack dev server settings

--- config/buildWebpackConfig.ts
+++ config/buildWebpackConfig.ts
@@ -1,13 +1,14 @@
 import { buildLoader } from "./buildLoader";
 import { buildPlugins } from "./buildPlugins";
 import { buildResolve } from "./buildResolve";
+import { buildDevServer } from "./buildDevServer";
 import { BuildOptions } from "./types/config";
 import webpack from "webpack";
 
 export function buildWebpackConfig(
   options: BuildOptions
 ): webpack.Configuration {
-  const { mode, paths } = options;
+  const { mode, paths, isDev } = options;
   return {
     mode: mode,
     entry: paths.entry,
@@ -21,5 +22,7 @@ export function buildWebpackConfig(
       rules: buildLoader(),
     },
     resolve: buildResolve(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,54 +1,22 @@
-// import webpack from "webpack";
-// import { buildWebpackConfig } from "./config/buildWebpackConfig";
-// import { BuildPaths } from "./config/types/config";
-// import path from "path";
-
-// const config: webpack.Configuration = {
-//     mode: 'production',
-//     entry: path.resolve(__dirname, 'src', 'index.tsx'),
-//     output: {
-//         path: path.resolve(__dirname, 'build'),
-//         filename: '[name].[contenthash].js',
-//         clean: true
-//     },
-//     plugins: [
-//         new HtmlWebpackPlugin({
-//             template: path.resolve(__dirname, 'public', 'index.html')
-//         }),
-//         new webpack.ProgressPlugin()
-//     ],
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.tsx?$/,
-//                 use: 'ts-loader',
-//                 exclude: /node_modules/
-//             }
-//         ]
-//     },
-//     resolve: {
-//         extensions: ['.tsx', '.ts', '.js']
-//     },
-//     devtool: 'inline-source-map',
-//     devServer: {
-//         port: 3000
-//     }
-// }
-// export default config;
 import webpack from "webpack";
 import { buildWebpackConfig } from "./config/buildWebpackConfig";
-import { BuildPaths } from "./config/types/config";
+import { BuildPaths, BuildEnv } from "./config/types/config";
 import path from "path";
-const paths: BuildPaths = {
-  entry: path.resolve(__dirname, "src", "index.tsx"),
-  build: path.resolve(__dirname, "build"),
-  html: path.resolve(__dirname, "public", "index.html"),
+
+export default (env: BuildEnv) => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+  };
+  const mode = env.mode || "development";
+  const isDev = mode === "development";
+  const PORT = env.port || 3000;
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  });
+  return config;
 };
-const mode = "development";
-const isDev = mode === "development";
-const config: webpack.Configuration = buildWebpackConfig({
-  mode: "development",
-  paths,
-  isDev,
-});
-export default config;
