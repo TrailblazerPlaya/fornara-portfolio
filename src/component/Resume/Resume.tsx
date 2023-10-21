6c867c3dfcf26158505fb250056a24109c1d295a
fornara <fornarapro@gmail.com>
2023-10-21 15:03:42 +0300

    Add modal window for skills

--- src/component/Resume/Resume.tsx
+++ src/component/Resume/Resume.tsx
@@ -1,28 +1,51 @@
 import cls from "./Resume.module.scss";
-
+import { useState } from "react";
+import skills from "./data/SkillsData";
 export const Resume = () => {
+    const [selectedSkill, setSelectedSkill] = useState(null);
+  
+    const handleSkillClick = (skill: { name: string; description: string }) => {
+      setSelectedSkill(skill);
+    };
+  
+    const closeModal = () => {
+      setSelectedSkill(null);
+    };
+  
+    const handleOverlayClick = (event: any) => {
+      if (event.target === event.currentTarget) {
+        closeModal();
+      }
+    };
+  
     return (
-        <div className={cls.Resume}>
-            <div className={cls.ResumeAbout}>
-                <h3>Николай Чубаков</h3>
-                <p>Frontend Developer</p>
-                <p>Age: 22 y.o</p>
-                <p>Location: Russia, Saratov</p>
-            </div>
-            <div className={cls.ResumeSkills}>
-                <ul>
-                    <h4>Кликабельно</h4>
-                    <hr />
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>TypeScript</li>
-                    <li>Webpack</li>
-                    <li>Git</li>
-                    <li>Feature-sliced design</li>
-                </ul>
-            </div>
+      <div className={cls.Resume}>
+        <div className={cls.ResumeAbout}>
+          <h3>Николай Чубаков</h3>
+          <p>Frontend Developer</p>
+          <p>Age: 22 y.o</p>
+          <p>Location: Russia, Saratov</p>
         </div>
+        <div className={cls.ResumeSkills}>
+          <ul>
+            <h4>Кликабельно</h4>
+            <hr />
+            {skills.map((skill) => (
+              <li key={skill.name} onClick={() => handleSkillClick(skill)}>
+                {skill.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        {selectedSkill && (
+          <div className={cls.ModalOverlay} onClick={handleOverlayClick}>
+            <div className={cls.Modal}>
+              <h4>{selectedSkill.name}</h4>
+              <p>{selectedSkill.description}</p>
+              <button onClick={closeModal}>Закрыть</button>
+            </div>
+          </div>
+        )}
+      </div>
     );
-};
+  };
---
+++ src/component/Resume/data/SkillsData.js
@@ -0,0 +1,14 @@
+const skills = [
+  { name: "HTML", description: "Описание HTML" },
+  { name: "CSS", description: "Описание CSS" },
+  { name: "JavaScript", description: "Описание JavaScript" },
+  { name: "React", description: "Описание React" },
+  { name: "Redux", description: "Описание Redux" },
+  { name: "Jest", description: "Описание Jest" },
+  { name: "Git", description: "Описание Git" },
+  { name: "Future-sliced design", description: "Описание Future-sliced design" },
+  { name: "TypeScript", description: "Описание TypeScript" },
+  // Добавьте остальные навыки и их описания
+];
+
+export default skills;
