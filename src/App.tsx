771926ede25cbe5834782edbdfddd938022edb91
fornara <fornarapro@gmail.com>
2023-11-07 16:37:53 +0300

    Redisign start

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { Navbar } from "./component/Navbar/Navbar";
 import { Suspense } from "react";
 import "./index.scss";
-import { Background } from "./component/Background/Background";
-import { Resume } from "./component/Resume/Resume";
-import { Footer } from "./component/Footer/Footer";
 const App = () => {
     return (
         <div className={"app"}>
             <Suspense fallback={<div>Loading...</div>}>
-                <Background />
                 <div className={"container"}>
-                    <Navbar />
-                    <Resume />
-                    <Footer />
+                    <h1>Portfolio</h1>
                 </div>
             </Suspense>
         </div>
--- src/component/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import cls from "./Footer.module.scss";
-
-export const Footer = () => {
-  return (
-    <div className={cls.Footer}>
-      <p>prod by Fornara</p>
-    </div>
-  );
-};
